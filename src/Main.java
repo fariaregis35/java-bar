9062b150e4b57417e30d5c5f680198af7f68f9d3
fariaregis35 <fariaregis35@gmail.com>
2020-05-08 09:46:57 -0300

    Leitura dos dados

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		Scanner sc = new Scanner(System.in);
+		String sexo ;
+		int qt_cerveja, qt_refrigerante,qt_espetinho ;
+		double vl_cerveja, vl_refrigerante, vl_espetinho,
+		vl_ingresso, vl_couvert ;
+		
+		System.out.println("Sexo: ");
+		sexo = sc.next();
+		System.out.println("Quantidade de Cervejas: ");
+		qt_cerveja = sc.nextInt();
+		System.out.println("Quantidade de refrigerantes: ");
+		qt_refrigerante =sc.nextInt();
+		System.out.println("Quantidade de espetinhos: ");
+		qt_espetinho = sc.nextInt();
+		
+		
+		
+		
+		
+		
+		System.out.println("Sexo:" +sexo);
+		System.out.println("Quantidade de Cervejas: " + qt_cerveja );
+		System.out.println("Quantidade de refrigerantes: "+ qt_refrigerante);
+		System.out.println("quantidade de espetinhos: "+ qt_espetinho);
+		
 
 	}
 
